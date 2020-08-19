be3e78ad0116c1d0e9bbe7ae87ccd8ec94bf3543
Peter Guenther <peter@grandcircus.co>
2020-08-18 21:27:43 -0400

    ready for demo

--- src/app/donut-detail/donut-detail.component.ts
+++ src/app/donut-detail/donut-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DonutDetail } from '../interfaces/donut-detail';
+import { Donut } from '../interfaces/donut';
+import { DonutApiService } from '../donut-api.service';
 
 @Component({
   selector: 'app-donut-detail',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donut-detail.component.css']
 })
 export class DonutDetailComponent implements OnInit {
+  @Input() donut:Donut;
+  detail:DonutDetail;
 
-  constructor() { }
+  constructor(private donutService:DonutApiService) { }
 
   ngOnInit(): void {
+    this.donutService.getDonutDetail(this.donut.id).subscribe(
+      (data:DonutDetail) => this.detail = { ...data },
+      error => console.error(error)
+    );
   }
 
 }
